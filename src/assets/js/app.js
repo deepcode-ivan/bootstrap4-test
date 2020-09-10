543ec57ecf9136f87a6cb6874d89704319195716
ivan buzunov <buzivan@gmail.com>
2020-09-10 16:26:06 +0500

    first commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,6 @@
+//= ../../../node_modules/jquery/dist/jquery.js
+// = ../../../node_modules/@popperjs/core/dist/umd/popper.js
+//= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/tooltip.js
+//= ../../../node_modules/bootstrap/js/dist/popover.js
+//= ../../../node_modules/bootstrap/js/dist/collapse.js
